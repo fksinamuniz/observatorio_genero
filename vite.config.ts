d26a2b22066d29e9b557b9bc0e95274d97087290
Fksina <79289627+fksinamuniz@users.noreply.github.com>
2025-11-09 17:45:26 -0300

    Add base path configuration to Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/observatorio_genero/', // ADICIONE ESTA LINHA
+      base: '/observatorio_genero/', // Adicione esta linha!
       server: {
         port: 3000,
         host: '0.0.0.0',
